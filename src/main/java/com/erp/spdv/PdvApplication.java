355d1f3a2910353d3fe41b9ec6391a8795311f8d
Guilherme de Souza <guilermeleitaoo@gmail.com>
2025-04-16 19:53:03 -0300

    fix: update application.properties

--- src/main/java/com/erp/spdv/PdvApplication.java
+++ src/main/java/com/erp/spdv/PdvApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.erp.spdv.config.security.core.SecurityConstants;
+
 @SpringBootApplication
 @RestController
 public class PdvApplication {
--- src/main/java/com/erp/spdv/config/security/core/SecurityConstants.java
+++ src/main/java/com/erp/spdv/config/security/core/SecurityConstants.java
@@ -1,13 +1,12 @@
 package com.erp.spdv.config.security.core;
 
 public class SecurityConstants {
-	
+	// default secret
+	public static final String SECRET_KEY = "secret";
+	public static final long EXPIRATION_TIME = 900_000; // 15 mins
+	public static final String TOKEN_PREFIX = "Bearer ";
+	public static final String HEADER_STRING = "Authorization";
+	public static final String SIGN_UP_URL = "/api/users";
+	public static final Integer ID_SUBSTRING_AUTH_HEADER = 7;
 
-		  public static final String SECRET_KEY = "645267556B58703273357638792F423F4428472B4B6250655368566D59713374";
-		  public static final long EXPIRATION_TIME = 900_000; // 15 mins
-		  public static final String TOKEN_PREFIX = "Bearer ";
-		  public static final String HEADER_STRING = "Authorization";
-		  public static final String SIGN_UP_URL = "/api/users";
-		  public static final Integer ID_SUBSTRING_AUTH_HEADER = 7;
-		
 }
